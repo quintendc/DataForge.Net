633109fbe7653c01c6f7120fbe58fe687b62adf7
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-02-18 13:49:31 +0100

    [UPD] start project

---
+++ DataForge/DataForge/DataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataForge
+{
+    internal class DataStore
+    {
+
+        internal static readonly string[] names = new string[] { "Taylor", "Jordan", "Morgan", "Parker", "Casey", "Bailey", "Hayden", "Riley", "Avery", "Spencer", "Sidney", "Carson", "Emerson", "Tyler", "Cameron", "Blake", "Campbell", "Chandler", "Cooper", "Davis", "Ellis", "Everett", "Grey", "Hunter", "Jackson", "Kelly", "Kennedy", "Lane", "Leslie", "Logan", "Madison", "Mason", "McKenzie", "Monroe", "Payton", "Reagan", "Reese", "Sawyer", "Shelby", "Sullivan" };
+
+    }
+}
