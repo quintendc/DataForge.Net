51e9db252024938d97be1dd204b8f39129908133
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-02-18 14:18:56 +0100

    [WIP] Datastore

--- DataForge/DataForge/DataStore.cs
+++ DataForge/DataForge/DataStore.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace DataForge
 {
-    internal class DataStore
+    internal static class DataStore
     {
 
-        internal static readonly string[] names = new string[] { "Taylor", "Jordan", "Morgan", "Parker", "Casey", "Bailey", "Hayden", "Riley", "Avery", "Spencer", "Sidney", "Carson", "Emerson", "Tyler", "Cameron", "Blake", "Campbell", "Chandler", "Cooper", "Davis", "Ellis", "Everett", "Grey", "Hunter", "Jackson", "Kelly", "Kennedy", "Lane", "Leslie", "Logan", "Madison", "Mason", "McKenzie", "Monroe", "Payton", "Reagan", "Reese", "Sawyer", "Shelby", "Sullivan" };
+        //internal static readonly string[] names = new string[] { "Taylor", "Jordan", "Morgan", "Parker", "Casey", "Bailey", "Hayden", "Riley", "Avery", "Spencer", "Sidney", "Carson", "Emerson", "Tyler", "Cameron", "Blake", "Campbell", "Chandler", "Cooper", "Davis", "Ellis", "Everett", "Grey", "Hunter", "Jackson", "Kelly", "Kennedy", "Lane", "Leslie", "Logan", "Madison", "Mason", "McKenzie", "Monroe", "Payton", "Reagan", "Reese", "Sawyer", "Shelby", "Sullivan" };
+
+        internal static readonly string[] maleNames = new string[] { "William", "Jacob", "Ethan", "Michael", "Alexander", "Daniel", "Benjamin", "Samuel", "David", "Joseph", "James", "Matthew", "Elijah", "Anthony", "Joshua", "Andrew", "Christopher", "John", "Ryan", "Nicholas", "Tyler", "Brandon", "Zachary", "Cameron", "Kevin", "Jared", "Justin", "Lucas", "Thomas", "Jonathan", "Caleb", "Nathan", "Dylan", "Adam", "Aaron", "Peter", "George", "Henry", "Oliver", "Liam" };
+        
+        internal static readonly string[] femaleNames = new string[] { "Emma", "Olivia", "Sophia", "Ava", "Mia", "Isabella", "Charlotte", "Amelia", "Emily", "Abigail", "Madison", "Elizabeth", "Ella", "Victoria", "Grace", "Avery", "Lily", "Chloe", "Sofia", "Aubrey", "Hannah", "Natalie", "Addison", "Aria", "Aurora", "Camila", "Aaliyah", "Evelyn", "Aria", "Brielle", "Luna", "Stella", "Penelope", "Harper", "Aria", "Aurora", "Leah", "Nova", "Riley", "Zoe" };
+        
+        internal static readonly string[] neutralNames = new string[] { "Charlie", "Alex", "Robin", "Jordan", "Riley", "Avery", "Taylor", "Casey", "Skyler", "Hayden", "Bailey", "Morgan", "Mackenzie", "Harley", "Dakota", "Phoenix", "Angel", "River", "Jayden", "Avery", "Eli", "Logan", "Carter", "Parker", "Ashton", "Evan", "Drew", "Adrian", "Cameron", "Spencer", "Quinn", "Elliot", "Jamie", "Darian", "Reese", "Sawyer", "Finley", "Taylor", "Peyton", "Sage" };
+
+        internal static readonly string[] lastNames = new string[] { "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "King", "Wright", "Scott", "Green", "Baker", "Adams", "Nelson", "Carter", "Mitchell", "Perez", "Roberts", "Turner", "Phillips" };
+
+        internal static readonly string[] streetNames = new string[] { "Main Street", "High Street", "Park Avenue", "Church Street", "Elm Street", "Oak Street", "Maple Street", "Hill Street", "Washington Street", "Broad Street", "Market Street", "Fifth Avenue", "Broadway", "King Street", "Queen Street", "George Street", "Victoria Road", "Oxford Street", "Regent Street", "Bond Street", "Baker Street", "Fleet Street", "Downing Street", "Lombard Street", "Abbey Road", "Carnaby Street", "Savile Row", "Piccadilly Circus", "Trafalgar Square", "Leicester Square", "Strand", "Pall Mall", "Portobello Road", "Shoreditch High Street", "Brick Lane", "Camden High Street", "Kensington High Street", "Brompton Road", "Notting Hill Gate", "Knightsbridge" };
+
+        internal static readonly string[] emailDomains = new string[] { "gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "aol.com", "icloud.com", "protonmail.com", "yandex.com", "mail.com", "gmx.com", "zoho.com", "tutanota.com", "live.com", "fastmail.com", "hushmail.com", "inbox.com", "aim.com", "mail.ru", "gmx.us", "me.com", "netscape.net", "comcast.net", "verizon.net", "sbcglobal.net", "att.net", "charter.net", "earthlink.net", "cox.net", "qq.com", "sina.com" };
 
     }
 }
