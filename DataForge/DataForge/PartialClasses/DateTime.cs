ce022fc7c9aeafb72520c93902092ee4c999771f
Quinten De Clerck <quinten.de.clerck@outlook.com>
2023-03-14 17:14:08 +0100

    [UPD] random in DateTime and Utils partial classes

--- DataForge/DataForge/PartialClasses/DateTime.cs
+++ DataForge/DataForge/PartialClasses/DateTime.cs
@@ -12,11 +12,11 @@ namespace DataForge
             public static System.DateTime GenerateRandomDateTimeBetweenYears(int startYear, int endYear)
             {
                 // generate a random year between startyear and endyear(included)
-                int year = new Random().Next(startYear, endYear + 1);
+                int year = random.Next(startYear, endYear + 1);
 
                 // generate a random month and day of the month
-                int month = new Random().Next(1, 13);
-                int day = new Random().Next(1, System.DateTime.DaysInMonth(year, month) + 1);
+                int month = random.Next(1, 13);
+                int day = random.Next(1, System.DateTime.DaysInMonth(year, month) + 1);
 
                 // return a new DateTime based on generated values
                 return new System.DateTime(year, month, day);
@@ -30,7 +30,7 @@ namespace DataForge
                 int ageRange = maxAge - minAge;
 
                 // generate a random year in the range betweeen ages
-                int randomAge = new Random().Next(ageRange + 1) + minAge;
+                int randomAge = random.Next(ageRange + 1) + minAge;
 
                 // Determine the date that corresponds to the generated number of years ago
                 System.DateTime datetime = System.DateTime.Now.AddYears(-randomAge);
--- DataForge/DataForge/PartialClasses/Utils.cs
+++ DataForge/DataForge/PartialClasses/Utils.cs
@@ -18,7 +18,6 @@ namespace DataForge
             public static T GetRandomEnumValue<T>() where T : struct, Enum
             {
                 var values = Enum.GetValues(typeof(T));
-                var random = new Random();
                 var randomEnumValue = (T)values.GetValue(random.Next(values.Length));
                 return randomEnumValue;
             }
